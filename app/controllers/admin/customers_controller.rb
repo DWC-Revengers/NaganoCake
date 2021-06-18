70990d1e38de3d3adefc2fb63e5cf231d794fbc2
EC2 Default User <ec2-user@ip-172-31-31-190.ec2.internal>
2021-06-18 10:13:06 +0000

    showアクション

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,11 +3,18 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+  
+  private
+  
+  def customer_parms
+    params.require(:customer).permit(:family_name,:first_name,:family_name_kana,:first_name_kana,:postal_code,:adress,:phone_number,:email,:is_deleted)
   end
 end
