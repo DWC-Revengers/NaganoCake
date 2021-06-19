42a716997868a09b1105f90a8677e4b11567b18f
Rintaro731 <matsurin-0605-blue@au.com>
2021-06-19 01:58:16 +0000

    会員ログイン機能

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -28,4 +28,8 @@ class Customers::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     public_root_path
   end
+
+  def after_sign_out_path_for(resource)
+    public_root_path
+  end
 end
