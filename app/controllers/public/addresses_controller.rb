7abccd7685be208ce56e7562c907f1a654ddb704
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-06-19 07:42:58 +0000

    配送先一覧/新規登録画面作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,14 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+    @address = Address.new
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    redirect_to public_addresses_path
   end
 
   def edit
@@ -13,4 +19,10 @@ class Public::AddressesController < ApplicationController
 
   def destroy
   end
+
+  private
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+
 end
