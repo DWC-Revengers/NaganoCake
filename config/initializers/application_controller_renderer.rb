a8df2317cb5d9e47707ae093f09b450a894543d7
niitsumario <xxx.ballet121.xxx@gmail.com>
2021-06-20 13:11:37 +0000

    adminのsweetsのshowページ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'b2c7093dfd959c350a963ac86f927cc62212807eae40811d58bd6a8664d6058c011ad405b7c2834f21cd85668b36974ddeb683d39338354eb7958667ebaefaa4'
