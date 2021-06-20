95fe170cd112b37a3812ac7166a577d39a60154b
Rintaro731 <matsurin-0605-blue@au.com>
2021-06-20 00:06:42 +0000

    会員編集ページ・編集機能の実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,14 +5,24 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to public_customer_path
   end
 
   def hide
   end
 
   def withdraw
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name,:first_name,:family_name_kana,:first_name_kana,:postal_code,:adress,:phone_number,:email)
   end
 end
