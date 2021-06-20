00f9111b285dfe133b3ce20a86a3bec5faa27669
niitsumario <xxx.ballet121.xxx@gmail.com>
2021-06-20 10:03:51 +0000

    管理者初期設定

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'e3fbaac5a3d7e7b58cf75bc50412f71eb523c4d2d6a9bf395551ceedb68a46b9f4fe5cb6a19b435562c1416ea6b8de081c80a30744bfb4c0ab8a228ea87fcae5'
